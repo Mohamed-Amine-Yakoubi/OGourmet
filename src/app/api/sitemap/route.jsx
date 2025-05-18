e5414fec32a1334e1c42d48cac978779e2fe10cd
Mohamed-Amine-Yakoubi <mohamedamineyakoubi1995@gmail.com>
2025-05-18 12:05:32 +0100

    final commit

--- src/app/api/sitemap/route.jsx
+++ src/app/api/sitemap/route.jsx
@@ -1,24 +1,27 @@
-// app/sitemap.ts
-export default function generateSitemap() {
-    const baseUrl = "https://ogourmet.com";
-  
-    return [
-      {
-        url: `${baseUrl}/`,
-        lastModified: new Date().toISOString(),
-      },
-      {
-        url: `${baseUrl}/Menu`,
-        lastModified: new Date().toISOString(),
-      },
-      {
-        url: `${baseUrl}/#Contact`,
-        lastModified: new Date().toISOString(),
-      },
-      {
-        url: `${baseUrl}/#À Propos`,
-        lastModified: new Date().toISOString(),
-      },
-    ];
-  }
-  
+import { NextResponse } from "next/server";
+import generateSitemap from "../../../lib/generate-sitemap";
+
+export async function GET() {
+  const sitemap = generateSitemap();
+
+  const xml = `
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${sitemap
+        .map(
+          (item) => `
+        <url>
+          <loc>${item.url}</loc>
+          <lastmod>${item.lastModified}</lastmod>
+        </url>
+      `
+        )
+        .join("")}
+    </urlset>
+  `.trim();
+
+  return new NextResponse(xml, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
+}
